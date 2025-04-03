5e49aae51d71caad14f04e836483f8dd13d15ff8
bypassaicom1 <2968897259@qq.com>
2025-04-03 14:08:14 +0800

    2025.4.3 14:08

--- src/components/select/category.vue
+++ src/components/select/category.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="category-container">
-        
+        <h2>分类</h2>
+        <div class="category-container__raw" v-for="item in data" :key="item.id">
+            <div class="category-container__raw__item" v-for="name in item.title" :key="name">
+                <span class="category-container__raw__item__text">{{ name}}</span>
+                <span>/</span>
+            </div>
+            
+           
+        </div>
     </div>
 </template>
 <script setup>
@@ -35,11 +43,315 @@ const data = [
                 ]
             }
         ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },
+    {
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },{
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },{
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
+    },{
+        id:1,
+        title:[
+            "电脑","配件","办公","文具"
+        ],
+        children:[
+            {
+                id:1,
+                title:'电脑整机',
+                desc:[
+                    "THISi ssdas111 ",
+                    "this isdsal; 222",
+                    "非常好324",
+                    "家具用哦噗3233WW"
+                ]
+            },
+            {
+                id:2,
+                title:'电脑配件',
+                desc:[
+                    "THISi ssdas ",
+                    "this isdsal; ",
+                    "非常好",
+                    "家具用哦噗"
+                ]
+            }
+        ]
     }
 ]
 </script>
 <style scoped lang="scss">
 .category-container{
-
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    &__raw{
+        padding: 0 20px;
+        border-radius:10px 0 0 10px;
+        height: 30px;
+        font-size: 14px;
+        display: flex;
+        justify-content:space-around;
+        align-items: center;
+        &:hover{
+            background-color: #FFFFFF;
+            transition: all 0.5s;
+        }
+        &__item{
+            display: flex;
+            justify-content: space-between;
+            gap: 15px;
+            &__text{
+                &:hover{
+                    transition: all 0.5s;
+                    color: red;
+                }
+            }
+        }   
+    }
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -17,11 +17,15 @@
                 <div class="container__top__category__left">
                     <Category></Category>
                 </div>
-                <div class="container__top__category__center">
-                    
-                </div>
+                <el-carousel class="container__top__category__center" :interval="5000" arrow="always">
+                    <el-carousel-item v-for="item in images" :key="item">
+                        <el-image style="width: 100%; height: 100%" :src="item" :zoom-rate="1.2" :max-scale="7"
+                            :min-scale="0.2" fit="cover" />
+                    </el-carousel-item>
+                </el-carousel>
                 <div class="container__top__category__right">
-                    
+                    <el-image v-for="item in cates" class="container__top__category__right__item" style="width: 100%; height: 100%" :src="item" :zoom-rate="1.2" :max-scale="7"
+                            :min-scale="0.2" fit="cover" />
                 </div>
             </div>
         </div>
@@ -34,6 +38,18 @@
 import Navigation from '@/components/tabar/navigation.vue'
 import Search from '@/components/input/search.vue'
 import Category from '@/components/select/category.vue'
+const images = [
+    "https://gw.alicdn.com/imgextra/i1/O1CN01NJPKCI1oN9KE4ry7R_!!6000000005212-1-tps-2112-800.gif",
+    "https://gw.alicdn.com/imgextra/i1/O1CN01CZVirQ29K2ZhxkHc4_!!6000000008048-1-tps-2112-800.gif",
+    "https://gw.alicdn.com/imgextra/i1/O1CN01q2nVLm1JqENxGdAV3_!!6000000001079-1-tps-2112-800.gif",
+    "https://gw.alicdn.com/imgextra/i2/O1CN01XNqbkw1syU4dGHy1b_!!6000000005835-1-tps-2112-800.gif"
+]
+const cates = [
+    "https://img.alicdn.com/imgextra/i2/O1CN01qceCje1JrbZc8E0Qi_!!6000000001082-2-tps-512-176.png",
+    "https://img.alicdn.com/imgextra/i3/O1CN016YKcXX28kJftC6PXB_!!6000000007970-2-tps-512-176.png",
+    "https://img.alicdn.com/imgextra/i1/O1CN01UoFupT1WnZ5J5rKta_!!6000000002833-2-tps-512-176.png",
+    "https://img.alicdn.com/imgextra/i1/O1CN01bWoqwD1nFzWEe55WO_!!6000000005061-2-tps-512-176.png"
+]
 </script>
 <style scoped lang="scss">
 .container {
@@ -47,7 +63,7 @@ import Category from '@/components/select/category.vue'
     &__top {
         flex: 3;
         width: 87%;
-       // background-color: red;
+        // background-color: red;
 
         &__search {
             height: 150px;
@@ -75,25 +91,41 @@ import Category from '@/components/select/category.vue'
                 background-size: auto 70px;
             }
         }
-        &__category{
+
+        &__category {
             height: 400px;
             display: flex;
             gap: 15px;
-            
-            &__left{
-                flex: 1.3;
-                background-color: aquamarine;
+
+            &__left {
+                flex: 0.9;
+                background-color: #F7F7F7;
                 border-radius: 10px;
+                padding: 0 0 0 10px;
             }
-            &__center{
-                flex: 2;
+
+            &__center {
+                :deep(.el-carousel__container){
+                    height: 100%;
+                }
+                height: 100%;
+                flex: 2.5;
                 background-color: aqua;
                 border-radius: 10px;
             }
-            &__right{
-                flex: 1.3;
-                background-color: blue;
+
+            &__right {
+                flex: 0.8;
                 border-radius: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                gap: 15px;
+                &__item{
+                    flex: 1;
+                    background-color: red;
+                    border-radius: 7px;
+                }
             }
         }
     }
