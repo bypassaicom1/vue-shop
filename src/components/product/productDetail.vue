e395a4082de08ae346860ba7cf18358ff7147b46
bypassaicom1 <2968897259@qq.com>
2025-04-07 18:02:32 +0800

    fix:详情页面进一步完成

--- src/components/product/productDetail.vue
+++ src/components/product/productDetail.vue
@@ -28,9 +28,53 @@
                             style="font-size: 20px;">{{ productInfo.zk_price }}</span>今日 20点开卖</span>
                     <span>美丽月</span>
                 </div>
-                <span><span style="color: #FF5000;font-size: 14px;font-family:PingFang SC;font-weight: bold;">券后¥<span style="font-size: 25px;">108</span></span><span style="color: rgb(122, 122, 122);font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;优惠前¥{{productInfo.price}} · 已售 {{productInfo.saleNum}}+</span></span>
+                <span><span style="color: #FF5000;font-size: 14px;font-family:PingFang SC;font-weight: bold;">券后¥<span
+                            style="font-size: 25px;">108</span></span><span
+                        style="color: rgb(122, 122, 122);font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;优惠前¥{{
+                            productInfo.price }}
+                        · 已售 {{ productInfo.saleNum }}+</span></span>
                 <div class="product-detail__container__detail__right__yhtag">
-                    <el-tag v-for="item in productInfo.coupon" size="large" :key="item" type="warning">{{item}}</el-tag>
+                    <el-tag v-for="item in productInfo.coupon" size="large" :key="item" type="warning">{{ item
+                    }}</el-tag>
+                </div>
+                <span style="font-weight: bold;"><span
+                        style="width: 56;height: 22px;text-align: justify;color: #959595;display: inline-block;font-size: 14px;margin-right: 20px;">配&nbsp;&nbsp;&nbsp;&nbsp;送</span>{{
+                            productInfo.delivery }}</span>
+                <span style="font-weight: bold;"><span
+                        style="width: 56;height: 22px;text-align: justify;color: #959595;display: inline-block;font-size: 14px;margin-right: 20px;">保&nbsp;&nbsp;&nbsp;&nbsp;障</span>{{
+                            productInfo.guarantee }}</span>
+                <span style="font-weight: bold;"><span
+                        style="width: 56;height: 22px;text-align: justify;color: #959595;display: inline-block;font-size: 14px;margin-right: 20px;">可选支付</span>{{
+                            productInfo.selectPay }}</span>
+                <div class="product-detail__container__detail__right__colorCate">
+                    <span
+                        style="width: 56;height: 22px;text-align: justify;color: #959595;display: inline-block;font-size: 14px;margin-right: 20px;font-weight: bold;">颜色分类</span>
+                    <div>
+                    </div>
+                    <div class="product-detail__container__detail__right__colorCate__area">
+                        <div v-for="item in productInfo.version[0].colorCategory" :key="item.id"
+                            class="product-detail__container__detail__right__colorCate__area__item">
+                            <img :src="item.img" alt="">
+                            <span>{{ item.name }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="product-detail__container__detail__right__nums">
+                    <span
+                        style="width: 56;height: 22px;text-align: justify;color: #959595;display: inline-block;font-size: 14px;margin-right: 20px;font-weight: bold;">数&nbsp;&nbsp;&nbsp;&nbsp;量</span>
+                    <el-input-number v-model="productNum" :min="1" :max="1000" @change="handleChange" />
+                </div>
+                <div class="product-detail__container__detail__right__add">
+                    <div class="product-detail__container__detail__right__add__tt1">
+                        <div class="product-detail__container__detail__right__add__tt1__t1"><span>立即购买</span></div>
+                        <div class="product-detail__container__detail__right__add__tt1__t2"><span>加入购物车</span></div>
+                    </div>
+                    <div class="product-detail__container__detail__right__add__sc">
+                        <el-icon size="20">
+                            <Star />
+                        </el-icon>
+                        <span>收藏</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -73,9 +117,21 @@ const productInfo = {
             id: 1,
             product: "入门款K3(3-5岁使用)",
             colorCategory: [
-                "苹果绿【共4枚原装软毛刷头+趣味贴纸】",
-                "柠檬黄【共4枚原装软毛刷头+趣味贴纸】",
-                "樱桃红【共4枚原装软毛刷头+趣味贴纸】"
+                {
+                    id: 1,
+                    name: '苹果绿【共4枚原装软毛刷头+趣味贴纸】',
+                    img: "https://gw.alicdn.com/bao/uploaded/i2/2218107642994/O1CN01lvDidv1XzIpY6zgFl_!!2218107642994.jpg_q30.jpg_.webp",
+                },
+                {
+                    id: 2,
+                    name: '苹果绿【共4枚原装软毛刷头+趣味贴纸】',
+                    img: "https://gw.alicdn.com/bao/uploaded/i2/2218107642994/O1CN01lvDidv1XzIpY6zgFl_!!2218107642994.jpg_q30.jpg_.webp",
+                },
+                {
+                    id: 3,
+                    name: '苹果绿【共4枚原装软毛刷头+趣味贴纸】',
+                    img: "https://gw.alicdn.com/bao/uploaded/i2/2218107642994/O1CN01lvDidv1XzIpY6zgFl_!!2218107642994.jpg_q30.jpg_.webp",
+                },
             ]
         },
         {
@@ -112,6 +168,7 @@ const productInfo = {
     ]
 }
 const ratePoint = ref(3.7)
+const productNum = ref(10)
 </script>
 <style lang="scss">
 .product-detail__container {
@@ -179,7 +236,7 @@ const ratePoint = ref(3.7)
             flex: 1;
             display: flex;
             flex-direction: column;
-            gap: 15px;
+            gap: 8px;
 
             &__tags {
                 display: flex;
@@ -200,9 +257,100 @@ const ratePoint = ref(3.7)
                 font-weight: bold;
                 color: white;
             }
-            &__yhtag{
+
+            &__yhtag {
                 display: flex;
                 gap: 10px;
+            }
+
+            &__colorCate {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 10px;
+
+                &__area {
+                    display: flex;
+                    flex-wrap: wrap;
+                    gap: 10px;
+
+                    &__item {
+                        padding: 5px;
+                        border: #DADDE0 solid 1px;
+                        border-radius: 10px;
+                        display: flex;
+                        align-items: center;
+                        gap: 8px;
+                        font-size: 14px;
+                        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+                        & img {
+                            width: 28px;
+                            height: 28px;
+                            border-radius: 5px;
+                        }
+
+                        &:hover {
+                            transition: all 0.3s;
+                            border: #FF5000 solid 1px;
+                            color: #FF5000;
+                        }
+                    }
+                }
+
+            }
+
+            &__nums {
+                .el-input-number {
+                    width: 110px;
+                }
+            }
+
+            &__add {
+                display: flex;
+                height: 40px;
+                margin-top: 20px;
+
+                &__tt1 {
+                    display: flex;
+                    width: 80%;
+                    margin-right: 20px;
+                    font-size: 15px;
+                    color: white;
+                    font-weight: bold;
+
+                    &__t1 {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        border-radius: 10px 0 0 10px;
+                        background-color: #FF6D00;
+                        width: 100%;
+                    }
+
+                    &__t2 {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        border-radius: 0 10px 10px 0;
+                        background-color: #FFA302;
+                        width: 100%;
+                    }
+                }
+
+                &__sc {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 2px;
+                    font-weight: bold;
+                    align-items: center;
+                    justify-content: center;
+                    margin-left: 10px;
+                    &:hover {
+                        transition: all 0.3s;
+                        color: #FFA302;
+                    }
+                }
+
             }
         }
 
